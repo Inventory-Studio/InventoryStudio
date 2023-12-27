6e4d07265e6f531c47a72d17a6f40188e3e2289b
1+1＞2 <lizhonghai@live.com>
2023-12-27 16:21:45 +0800

    add Company UI and Logic

---
+++ InventoryStudio/ISLibrary/CompanyFilter.cs
@@ -0,0 +1,16 @@
+﻿using CLRFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISLibrary
+{
+    public class CompanyFilter
+    {
+        public Database.Filter.StringSearch.SearchFilter CompanyID { get; set; }
+        public Database.Filter.StringSearch.SearchFilter ParentCompanyID { get; set; }
+        public Database.Filter.StringSearch.SearchFilter CreatedBy { get; set; }
+    }
+}
