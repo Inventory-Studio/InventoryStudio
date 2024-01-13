052d572d496e62bd26ad9fd751a0b17d6662f2c9
Sky Designs <sauldesigns@outlook.com>
2024-01-13 14:46:17 -0800

    Add total count to aspnetroles library

--- InventoryStudio/ISLibrary/AspNet/AspNetRoles.cs
+++ InventoryStudio/ISLibrary/AspNet/AspNetRoles.cs
@@ -14,7 +14,12 @@ namespace ISLibrary
     public class AspNetRoles : BaseClass
     {
         public string Id { get; set; }
-        public bool IsNew { get { return string.IsNullOrEmpty(Id); } }
+
+        public bool IsNew
+        {
+            get { return string.IsNullOrEmpty(Id); }
+        }
+
         public string Name { get; set; } = string.Empty;
         public string NormalizedName { get; set; } = string.Empty;
         public string ConcurrencyStamp { get; set; } = string.Empty;
@@ -24,13 +29,13 @@ namespace ISLibrary
         public DateTime CreatedOn { get; private set; }
 
         private List<string>? mUserIds = null;
+
         public List<string>? AssignUserIds
         {
             get
             {
                 if (mUserIds == null && !string.IsNullOrEmpty(Id))
                 {
-
                     try
                     {
                         mUserIds = GetAssignUserIds();
@@ -39,13 +44,14 @@ namespace ISLibrary
                     {
                         throw new Exception(ex.Message);
                     }
-
                 }
+
                 return mUserIds;
             }
         }
 
         private List<string>? mPermissionIds = null;
+
         public List<string>? AssignPermissionIds
         {
             get
@@ -67,11 +73,13 @@ namespace ISLibrary
                         objFilter = null;
                     }
                 }
+
                 return mPermissionIds;
             }
         }
 
         private List<AspNetPermission>? mPermissions = null;
+
         public List<AspNetPermission>? AssignPermissions
         {
             get
@@ -87,6 +95,7 @@ namespace ISLibrary
                         throw new Exception(ex.Message);
                     }
                 }
+
                 return mPermissions;
             }
         }
@@ -95,7 +104,7 @@ namespace ISLibrary
         {
         }
 
-        public AspNetRoles(/*string CompanyID,*/ string Id)
+        public AspNetRoles( /*string CompanyID,*/ string Id)
         {
             //this.CompanyID = CompanyID;
             this.Id = Id;
@@ -108,8 +117,7 @@ namespace ISLibrary
         }
 
         protected override void Load()
-        {           
-
+        {
             DataSet objData = null;
             string strSQL = string.Empty;
 
@@ -139,6 +147,7 @@ namespace ISLibrary
 
             base.Load();
         }
+
         private void Load(DataRow objRow)
         {
             DataColumnCollection objColumns = null;
@@ -150,10 +159,12 @@ namespace ISLibrary
                 if (objColumns.Contains("Id")) Id = Convert.ToString(objRow["Id"]);
                 if (objColumns.Contains("Name")) Name = Convert.ToString(objRow["Name"]);
                 if (objColumns.Contains("NormalizedName")) NormalizedName = Convert.ToString(objRow["NormalizedName"]);
-                if (objColumns.Contains("ConcurrencyStamp")) ConcurrencyStamp = Convert.ToString(objRow["ConcurrencyStamp"]);
+                if (objColumns.Contains("ConcurrencyStamp"))
+                    ConcurrencyStamp = Convert.ToString(objRow["ConcurrencyStamp"]);
                 if (objColumns.Contains("Discriminator")) Discriminator = Convert.ToString(objRow["Discriminator"]);
                 if (objColumns.Contains("CompanyId")) CompanyId = Convert.ToString(objRow["CompanyId"]);
-                if (objColumns.Contains("UpdatedOn") && objRow["UpdatedOn"] != DBNull.Value) UpdatedOn = Convert.ToDateTime(objRow["UpdatedOn"]);
+                if (objColumns.Contains("UpdatedOn") && objRow["UpdatedOn"] != DBNull.Value)
+                    UpdatedOn = Convert.ToDateTime(objRow["UpdatedOn"]);
                 if (objColumns.Contains("CreatedOn")) CreatedOn = Convert.ToDateTime(objRow["CreatedOn"]);
 
                 if (string.IsNullOrEmpty(Id)) throw new Exception("Missing Id in the datarow");
@@ -193,6 +204,7 @@ namespace ISLibrary
                 if (objConn != null) objConn.Dispose();
                 objConn = null;
             }
+
             return true;
         }
 
@@ -218,7 +230,8 @@ namespace ISLibrary
                 dicParam["UpdatedOn"] = DateTime.UtcNow;
                 dicParam["CreatedOn"] = DateTime.UtcNow;
 
-                Id = Database.ExecuteSQLWithIdentity(Database.GetInsertSQL(dicParam, "AspNetRoles"), objConn, objTran).ToString();
+                Id = Database.ExecuteSQLWithIdentity(Database.GetInsertSQL(dicParam, "AspNetRoles"), objConn, objTran)
+                    .ToString();
 
 
                 Load(objConn, objTran);
@@ -231,6 +244,7 @@ namespace ISLibrary
             {
                 dicParam = null;
             }
+
             return true;
         }
 
@@ -259,12 +273,12 @@ namespace ISLibrary
                 if (objConn != null) objConn.Dispose();
                 objConn = null;
             }
+
             return true;
         }
 
         public override bool Update(SqlConnection objConn, SqlTransaction objTran)
-        {          
-
+        {
             Hashtable dicParam = new Hashtable();
             Hashtable dicWParam = new Hashtable();
 
@@ -327,6 +341,7 @@ namespace ISLibrary
                 if (objConn != null) objConn.Dispose();
                 objConn = null;
             }
+
             return true;
         }
 
@@ -351,6 +366,7 @@ namespace ISLibrary
             {
                 dicDParam = null;
             }
+
             return true;
         }
 
@@ -386,6 +402,7 @@ namespace ISLibrary
             {
                 objAspNetRoless = null;
             }
+
             return objReturn;
         }
 
@@ -401,12 +418,14 @@ namespace ISLibrary
             return GetAspNetRoless(CompanyID, Filter, null, null, out intTotalCount);
         }
 
-        public static List<AspNetRoles> GetAspNetRoless(string CompanyID, AspNetRolesFilter Filter, int? PageSize, int? PageNumber, out int TotalRecord)
+        public static List<AspNetRoles> GetAspNetRoless(string CompanyID, AspNetRolesFilter Filter, int? PageSize,
+            int? PageNumber, out int TotalRecord)
         {
             return GetAspNetRoless(CompanyID, Filter, string.Empty, true, PageSize, PageNumber, out TotalRecord);
         }
 
-        public static List<AspNetRoles> GetAspNetRoless(string CompanyID, AspNetRolesFilter Filter, string SortExpression, bool SortAscending, int? PageSize, int? PageNumber, out int TotalRecord)
+        public static List<AspNetRoles> GetAspNetRoless(string CompanyID, AspNetRolesFilter Filter,
+            string SortExpression, bool SortAscending, int? PageSize, int? PageNumber, out int TotalRecord)
         {
             List<AspNetRoles> objReturn = null;
             AspNetRoles objNew = null;
@@ -427,7 +446,12 @@ namespace ISLibrary
                     if (Filter.Name != null) strSQL += Database.Filter.StringSearch.GetSQLQuery(Filter.Name, "Name");
                 }
 
-                if (PageSize != null && PageNumber != null) strSQL = Database.GetPagingSQL(strSQL, string.IsNullOrEmpty(SortExpression) ? "Id" : Utility.CustomSorting.GetSortExpression(typeof(AspNetRoles), SortExpression), string.IsNullOrEmpty(SortExpression) ? false : SortAscending, PageSize.Value, PageNumber.Value);
+                if (PageSize != null && PageNumber != null)
+                    strSQL = Database.GetPagingSQL(strSQL,
+                        string.IsNullOrEmpty(SortExpression)
+                            ? "Id"
+                            : Utility.CustomSorting.GetSortExpression(typeof(AspNetRoles), SortExpression),
+                        string.IsNullOrEmpty(SortExpression) ? false : SortAscending, PageSize.Value, PageNumber.Value);
                 objData = Database.GetDataSet(strSQL);
 
                 if (objData != null && objData.Tables[0].Rows.Count > 0)
@@ -439,6 +463,8 @@ namespace ISLibrary
                         objReturn.Add(objNew);
                     }
                 }
+
+                TotalRecord = objReturn.Count();
             }
             catch (Exception ex)
             {
@@ -448,20 +474,22 @@ namespace ISLibrary
             {
                 objData = null;
             }
+
             return objReturn;
         }
 
         public List<string> GetAssignUserIds()
         {
-            List<string> objReturn = new List<string>(); ;
+            List<string> objReturn = new List<string>();
+            ;
             DataSet objData = null;
             string strSQL = string.Empty;
 
             try
             {
                 strSQL = "SELECT s.UserId " +
-                        "FROM AspNetUserRoles (NOLOCK) s " +
-                        " where RoleId = " + Id;
+                         "FROM AspNetUserRoles (NOLOCK) s " +
+                         " where RoleId = " + Id;
 
                 objData = Database.GetDataSet(strSQL);
 
@@ -475,7 +503,6 @@ namespace ISLibrary
                         {
                             objReturn.Add(Convert.ToString(objRow["UserId"]));
                         }
-
                     }
                 }
             }
@@ -487,20 +514,22 @@ namespace ISLibrary
             {
                 objData = null;
             }
+
             return objReturn;
         }
 
         public List<string> GetAssignPermissionIds()
         {
-            List<string> objReturn = new List<string>(); ;
+            List<string> objReturn = new List<string>();
+            ;
             DataSet objData = null;
             string strSQL = string.Empty;
 
             try
             {
                 strSQL = "SELECT s.PermissionId " +
-                        "FROM AspNetRolePermission (NOLOCK) s " +
-                        " where RoleId = " + Id;
+                         "FROM AspNetRolePermission (NOLOCK) s " +
+                         " where RoleId = " + Id;
 
                 objData = Database.GetDataSet(strSQL);
 
@@ -514,7 +543,6 @@ namespace ISLibrary
                         {
                             objReturn.Add(Convert.ToString(objRow["PermissionId"]));
                         }
-
                     }
                 }
             }
@@ -526,12 +554,14 @@ namespace ISLibrary
             {
                 objData = null;
             }
+
             return objReturn;
         }
 
         public List<AspNetPermission> GetAssignPermissions()
         {
-            List<AspNetPermission> objReturn = new List<AspNetPermission>(); ;
+            List<AspNetPermission> objReturn = new List<AspNetPermission>();
+            ;
             AspNetPermission objNew = null;
             DataSet objData = null;
             string strSQL = string.Empty;
@@ -539,9 +569,9 @@ namespace ISLibrary
             try
             {
                 strSQL = "SELECT s.* " +
-                        "FROM AspNetPermission (NOLOCK) s " +
-                        "INNER JOIN  AspNetRolePermission ap on ap.PermissionId = s.PermissionId" +
-                        " where ap.RoleId = " + Id;
+                         "FROM AspNetPermission (NOLOCK) s " +
+                         "INNER JOIN  AspNetRolePermission ap on ap.PermissionId = s.PermissionId" +
+                         " where ap.RoleId = " + Id;
 
                 objData = Database.GetDataSet(strSQL);
 
@@ -562,6 +592,7 @@ namespace ISLibrary
             {
                 objData = null;
             }
+
             return objReturn;
         }
     }
