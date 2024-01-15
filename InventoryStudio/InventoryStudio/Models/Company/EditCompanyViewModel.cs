6ed351ffed088eefd3f688e3a2227793780dd37f
skydesigns <sauldesigns@outlook.com>
2024-01-14 23:38:11 -0800

    Convert Company Edit Page to Template style

--- InventoryStudio/InventoryStudio/Models/Company/EditCompanyViewModel.cs
+++ InventoryStudio/InventoryStudio/Models/Company/EditCompanyViewModel.cs
@@ -1,21 +1,27 @@
-﻿namespace InventoryStudio.Models.Company
+﻿using System.ComponentModel;
+
+namespace InventoryStudio.Models.Company
 {
     public class EditCompanyViewModel
     {
         public string CompanyID { get; set; }
 
-        public string CompanyName { get; set; }
+        [DisplayName("Company Name")] public string CompanyName { get; set; }
 
-        public bool AutomateFulfillment { get; set; }
+        [DisplayName("Automate Fulfillment")] public bool AutomateFulfillment { get; set; }
 
-        public string ShippoAPIKey { get; set; }
+        [DisplayName("Shippo API Key")] public string ShippoAPIKey { get; set; }
 
+        [DisplayName("Include Packing Slip On Label")]
         public bool IncludePackingSlipOnLabel { get; set; }
 
+        [DisplayName("Default Fulfillment Method")]
         public string DefaultFulfillmentMethod { get; set; }
 
+        [DisplayName("Default Fulfillment Strategy")]
         public string DefaultFulfillmentStrategy { get; set; }
 
+        [DisplayName("Default Allocation Strategy")]
         public string DefaultAllocationStrategy { get; set; }
     }
 }
