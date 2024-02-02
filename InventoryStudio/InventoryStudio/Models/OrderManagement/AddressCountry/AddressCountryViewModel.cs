7305f3f51e29ed642e6500763ee10ee6a3927862
skydesigns <sauldesigns@outlook.com>
2024-02-01 19:21:18 -0800

    Added display names to Address Country Models

--- InventoryStudio/InventoryStudio/Models/OrderManagement/AddressCountry/AddressCountryViewModel.cs
+++ InventoryStudio/InventoryStudio/Models/OrderManagement/AddressCountry/AddressCountryViewModel.cs
@@ -1,16 +1,20 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace InventoryStudio.Models.OrderManagement.AddressCountry
 {
     public class AddressCountryViewModel
     {
+        [DisplayName("Country ID")]
         public string CountryID { get; set; } = null!;
-
       
+        [DisplayName("Country Name")]
         public string CountryName { get; set; } = null!;
 
+        [DisplayName("USPS Country Name")]
         public string USPSCountryName { get; set; } = null!;
 
+        [DisplayName("Is Eligible For PLT FedEx")]
         public bool IsEligibleForPLTFedEX { get; set; }
 
         public string? EelPfc { get; set; }
