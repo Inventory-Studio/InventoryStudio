4c4d3024ed015e148857f2652d35562e24d5ea00
skydesigns <sauldesigns@outlook.com>
2024-02-01 19:38:10 -0800

    Customer create, details to template style

--- InventoryStudio/InventoryStudio/Models/OrderManagement/Customer/CustomerViewModel.cs
+++ InventoryStudio/InventoryStudio/Models/OrderManagement/Customer/CustomerViewModel.cs
@@ -1,29 +1,43 @@
-﻿namespace InventoryStudio.Models.OrderManagement.Customer
+﻿using System.ComponentModel;
+
+namespace InventoryStudio.Models.OrderManagement.Customer
 {
     public class CustomerViewModel
     {
+        [DisplayName("Customer ID")]
         public string CustomerID { get; set; }
 
+        [DisplayName("Company")]
         public string Company { get; set; }
 
+        [DisplayName("Client")]
         public string Client { get; set; }
 
+        [DisplayName("Company Name")]
         public string? CompanyName { get; set; }
 
+        [DisplayName("First Name")]
         public string? FirstName { get; set; }
 
+        [DisplayName("Last Name")]
         public string? LastName { get; set; }
 
+        [DisplayName("E-mail")]
         public string? EmailAddress { get; set; }
 
+        [DisplayName("External ID")]
         public string? ExternalID { get; set; }
 
+        [DisplayName("Updated By")]
         public string UpdatedBy { get; set; }
 
+        [DisplayName("Updated On")]
         public DateTime? UpdatedOn { get; set; }
 
+        [DisplayName("Created By")]
         public string CreatedBy { get; set; }
 
+        [DisplayName("Created On")]
         public DateTime CreatedOn { get; set; }
     }
 }
--- InventoryStudio/InventoryStudio/Models/OrderManagement/Customer/EditCustomerViewModel.cs
+++ InventoryStudio/InventoryStudio/Models/OrderManagement/Customer/EditCustomerViewModel.cs
@@ -1,21 +1,31 @@
-﻿namespace InventoryStudio.Models.OrderManagement.Customer
+﻿using System.ComponentModel;
+
+namespace InventoryStudio.Models.OrderManagement.Customer
 {
     public class EditCustomerViewModel
     {
+        [DisplayName("Customer ID")]
         public string CustomerID { get; set; }
 
+        [DisplayName("Company ID")]
         public string CompanyID { get; set; }
 
+        [DisplayName("Client ID")]
         public string? ClientID { get; set; }
 
+        [DisplayName("Company Name")]
         public string? CompanyName { get; set; }
 
+        [DisplayName("First Name")]
         public string? FirstName { get; set; }
 
+        [DisplayName("Last Name")]
         public string? LastName { get; set; }
 
+        [DisplayName("E-mail")]
         public string? EmailAddress { get; set; }
 
+        [DisplayName("External ID")]
         public string? ExternalID { get; set; }
 
     }
