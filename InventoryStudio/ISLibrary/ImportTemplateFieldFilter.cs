96ca7ffd577a397a313c951a2f9af9f56edd5726
1+1＞2 <lizhonghai@live.com>
2024-02-08 16:16:54 +0800

    add ImportTemplate Table crud

---
+++ InventoryStudio/ISLibrary/ImportTemplateFieldFilter.cs
@@ -0,0 +1,20 @@
+﻿using CLRFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISLibrary
+{
+    public class ImportTemplateFieldFilter
+    {
+        public Database.Filter.StringSearch.SearchFilter ImportTemplateID { get; set; }
+
+        public Database.Filter.StringSearch.SearchFilter SourceField { get; set; }
+
+        public Database.Filter.StringSearch.SearchFilter DestinationTable { get; set; }
+
+        public Database.Filter.StringSearch.SearchFilter DestinationField { get; set; }
+    }
+}
