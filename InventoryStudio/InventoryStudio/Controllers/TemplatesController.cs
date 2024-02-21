fd2580b53b25cda5c989dc881c5a1e336dcd1c38
1+1＞2 <lizhonghai@live.com>
2024-02-21 17:49:58 +0800

    add Templates  controller logic and ui

--- InventoryStudio/InventoryStudio/Controllers/TemplatesController.cs
+++ InventoryStudio/InventoryStudio/Controllers/TemplatesController.cs
@@ -9,69 +9,168 @@ namespace InventoryStudio.Controllers
 {
     public class TemplatesController : Controller
     {
-        public async Task<IActionResult> Index()
+        private readonly string CompanyID = string.Empty;
+
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public TemplatesController(IHttpContextAccessor httpContextAccessor)
         {
-            Claim? company = User.Claims.FirstOrDefault(t => t.Type == "CompanyId");
-            return View();
+            _httpContextAccessor = httpContextAccessor;
+            var user = _httpContextAccessor.HttpContext?.User;
+            if (user != null && user.Identity.IsAuthenticated)
+            {
+                Claim? company = user.Claims.FirstOrDefault(t => t.Type == "CompanyId");
+                if (company != null)
+                    CompanyID = company.Value;
+            }
+        }
+        public IActionResult Index()
+        {
+            var templates = ImportTemplate.GetImportTemplates(CompanyID);
+            var list = new List<TemplateViewModel>();
+            foreach (var template in templates)
+            {
+                list.Add(EntityConvertViewModel(template));
+            }
+            return View(list);
+        }
+
+        private TemplateViewModel EntityConvertViewModel(ImportTemplate template)
+        {
+            var viewModel = new TemplateViewModel();
+            viewModel.ImportTemplateID = template.ImportTemplateID;
+            if (!string.IsNullOrEmpty(template.CompanyID))
+            {
+                var company = new Company(template.CompanyID);
+                if (company != null)
+                    viewModel.Company = company.CompanyName;
+            }
+            viewModel.TemplateName = template.TemplateName;
+            viewModel.Type = template.Type;
+            viewModel.ImportType = template.ImportType;
+            if (!string.IsNullOrEmpty(template.UpdatedBy))
+            {
+                var user = new AspNetUsers(template.UpdatedBy);
+                if (user != null)
+                    viewModel.UpdatedBy = user.UserName;
+            }
+            viewModel.UpdatedOn = template.UpdatedOn;
+            if (!string.IsNullOrEmpty(template.CreatedBy))
+            {
+                var user = new AspNetUsers(template.CreatedBy);
+                if (user != null)
+                    viewModel.CreatedBy = user.UserName;
+            }
+            viewModel.CreatedOn = template.CreatedOn;
+            return viewModel;
         }
 
 
-        public async Task<IActionResult> Details(string id)
+        public IActionResult Details(string id)
         {
             if (id == null)
-            {
                 return NotFound();
-            }
-            return View();
+            var template = new ImportTemplate(CompanyID, id);
+            if (template == null)
+                return NotFound();
+            var viewModel = EntityConvertViewModel(template);
+            return View(viewModel);
         }
 
         public IActionResult Create()
         {
+            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            var user = new AspNetUsers(userId);
+            var companies = user.Companies;
+            ViewData["CompanyID"] = new SelectList(companies, "CompanyID", "CompanyName");
             return View();
         }
 
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("ImportTemplateID,CompanyID,TemplateName,Type,ImportType,UpdatedBy,UpdatedOn,CreatedBy,CreatedOn")] CreateTemplateViewModel input)
+        public IActionResult Create([Bind("ImportTemplateID,CompanyID,TemplateName,Type,ImportType,UpdatedBy,UpdatedOn,CreatedBy,CreatedOn")] CreateTemplateViewModel input)
         {
+            if (ModelState.IsValid)
+            {
+                var template = new ImportTemplate();
+                template.CompanyID = input.CompanyID;
+                template.TemplateName = input.TemplateName;
+                template.Type = input.Type;
+                template.CreatedBy = User.FindFirstValue(ClaimTypes.NameIdentifier);
+                template.Create();
+            }
+            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            var user = new AspNetUsers(userId);
+            var companies = user.Companies;
+            ViewData["CompanyID"] = new SelectList(companies, "CompanyID", "CompanyName", input.CompanyID);
             return View();
         }
 
-        public async Task<IActionResult> Edit(string id)
+        public IActionResult Edit(string id)
         {
             if (id == null)
-            {
                 return NotFound();
-            }
-
-            return View();
+            var template = new ImportTemplate(CompanyID, id);
+            if (template == null)
+                return NotFound();
+            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            var user = new AspNetUsers(userId);
+            var companies = user.Companies;
+            ViewData["CompanyID"] = new SelectList(companies, "CompanyID", "CompanyName");
+            var viewModel = new EditTemplateViewModel();
+            viewModel.ImportTemplateID = template.ImportTemplateID;
+            viewModel.CompanyID = template.CompanyID;
+            viewModel.TemplateName = template.TemplateName;
+            viewModel.Type = template.Type;
+            viewModel.ImportType = template.ImportType;
+            return View(viewModel);
         }
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(string id, [Bind("ImportTemplateID,CompanyID,TemplateName,Type,ImportType,UpdatedBy,UpdatedOn,CreatedBy,CreatedOn")] EditTemplateViewModel input)
+        public IActionResult Edit(string id, [Bind("ImportTemplateID,CompanyID,TemplateName,Type,ImportType,UpdatedBy,UpdatedOn,CreatedBy,CreatedOn")] EditTemplateViewModel input)
         {
             if (id != input.ImportTemplateID)
-            {
                 return NotFound();
+            if (ModelState.IsValid)
+            {
+                var template = new ImportTemplate(CompanyID, id);
+                if (template == null)
+                    return NotFound();
+                template.CompanyID = input.CompanyID;
+                template.TemplateName = template.TemplateName;
+                template.Type = template.Type;
+                template.ImportType = template.ImportType;
+                template.UpdatedBy = User.FindFirstValue(ClaimTypes.NameIdentifier);
+                template.Update();
+                return RedirectToAction(nameof(Index));
             }
+            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            var user = new AspNetUsers(userId);
+            var companies = user.Companies;
+            ViewData["CompanyID"] = new SelectList(companies, "CompanyID", "CompanyName", input.CompanyID);
             return View(input);
         }
 
-        public async Task<IActionResult> Delete(string? id)
+        public IActionResult Delete(string? id)
         {
             if (id == null)
-            {
                 return NotFound();
-            }
-            return View();
+            var template = new ImportTemplate(CompanyID, id);
+            if (template == null)
+                return NotFound();
+            var viewModel = EntityConvertViewModel(template);
+            return View(viewModel);
         }
 
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(string id)
+        public IActionResult DeleteConfirmed(string id)
         {
+            var template = new ImportTemplate(CompanyID, id);
+            if (template != null)
+                template.Delete();
             return RedirectToAction(nameof(Index));
         }
 
