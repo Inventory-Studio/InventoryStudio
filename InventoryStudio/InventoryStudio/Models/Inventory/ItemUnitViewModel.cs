e55cf15c6a0b8e571679536e974a38bc4ca6420a
will <wucangzhou@gmail.com>
2024-03-01 16:17:11 +0800

    ItemUnit Create and Detail function

---
+++ InventoryStudio/InventoryStudio/Models/Inventory/ItemUnitViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using ISLibrary;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+
+namespace InventoryStudio.Models
+{
+    public class ItemUnitViewModel
+    {
+        public string Name { get; set; }
+
+        [ValidateNever]
+        public List<ItemUnit> ItemUnits { get; set; }
+       
+    }
+}
