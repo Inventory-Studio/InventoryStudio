9884c9f0c6519954c0f0a390335287b34dc8d1b2
skydesigns <sauldesigns@outlook.com>
2024-03-05 00:14:04 -0800

    item create update

--- InventoryStudio/InventoryStudio/wwwroot/js/utils/utils.js
+++ InventoryStudio/InventoryStudio/wwwroot/js/utils/utils.js
@@ -46,3 +46,44 @@ function customExportToolbarClick(gridId, fileName) {
         }
     }
 }
+function customBarcodeExportToolbarClick(gridId, fileName) {
+    return (args) => {
+        var gridObj = document.getElementById(gridId).ej2_instances[0];
+        const gridPdfExport = gridId + "_pdfexport";
+        const gridExcelExport = gridId + "_excelexport";
+        if (args.item.id === gridPdfExport) {
+            var pdfdata = gridObj.getSelectedRecords();
+            if (pdfdata?.length > 0) {
+                var exportProperties = {
+                    dataSource: pdfdata,
+                    includeHiddenColumn: true,
+                    fileName: fileName + ".pdf"
+                };
+                gridObj.pdfExport(exportProperties);
+            } else {
+                gridObj.pdfExport({
+                    dataSource: gridObj.dataSource,
+                    fileName: fileName + ".pdf"
+                });
+            }
+        }
+        if (args.item.id === gridExcelExport) {
+            var exceldata = gridObj.getSelectedRecords();
+            if (exceldata?.length > 0) {
+                var exportProperties = {
+                    dataSource: exceldata,
+                    includeHiddenColumn: true,
+                    fileName: fileName + ".xlsx"
+                };
+                gridObj.excelExport(exportProperties);
+            } else {
+                gridObj.excelExport({
+                    dataSource: gridObj.dataSource,
+                    fileName: fileName + ".xlsx"
+                });
+
+            }
+        }
+        console.log(args)
+    }
+}
